cc5db2f9956d14b4945262d4dd04022dd9cde424
Abel <abochoa91@gmail.com>
2023-09-21 10:09:48 -0400

    create basic spring project structure

---
+++ src/main/java/com/abel/drones/DronesDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.abel.drones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abel/drones/DronesDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abel.drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
